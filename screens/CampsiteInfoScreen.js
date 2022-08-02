ee5a7082ef163ed38c3740876461d51b71a0e45f
aarontorresweb <aarontorresweb@gmail.com>
2022-08-01 20:21:18 -0700

    Week 2 Workshop Task 2 Complete

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,4 +1,5 @@
 import { FlatList, StyleSheet, Button, Modal, Text, View } from 'react-native';
+import { Input, Rating } from 'react-native-elements'
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import RenderCampsite from '../features/campsites/RenderCampsite';
@@ -9,13 +10,39 @@ const CampsiteInfoScreen = ({ route }) => {
     const comments = useSelector((state) => state.comments);
     const favorites = useSelector((state) => state.favorites);
     const [showModal, setShowModal] = useState(false);
+    const [rating, setRating] = useState(5);
+    const [author, setAuthor] = useState('');
+    const [text, setText] = useState('');
     const dispatch = useDispatch();
+
+    const handleSubmit = () => {
+        const newComment = {
+            author,
+            rating,
+            text,
+            campsiteId: campsite.id
+        };
+        console.log(newComment);
+        console.log('Submission Successful');
+        setShowModal(!showModal);
+    }
+
+    const resetForm = () => {
+        setRating(5),
+        setAuthor(''),
+        setText('')
+    }
 
     const renderCommentItem = ({ item }) => {
         return (
             <View style={styles.commentItem}>
                 <Text style={{ fontSize: 14 }}>{item.text}</Text>
-                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Rating 
+                    startingValue={item.rating}
+                    itemSize={10}
+                    style={{alignItems: 'flex-start', 
+                    paddingVertical: '5%'}}
+                />
                 <Text style={{ fontSize: 12 }}>
                     {`-- ${item.author}, ${item.date}`}
                 </Text>
@@ -53,9 +80,45 @@ const CampsiteInfoScreen = ({ route }) => {
                 visible={showModal}
                 onRequestClose={() => setShowModal(!showModal)}
             >
+                <View style={styles.modal}>
+                    <Rating 
+                        showRating
+                        startingValue={rating}
+                        imageSize={40}
+                        onFinishRating={(rating) => setRating(rating)}
+                        style={{paddingVertical: 10}}
+                    />
+                    <Input 
+                        placeholder='Author'
+                        leftIcon={{ type: 'font-awesome', name:'user-o'}}
+                        leftIconContainerStyle={{ paddingRight: 10 }}
+                        onChangeText={(author) => setAuthor(author)}
+                        value={author}
+                    />
+                    <Input 
+                        placeHolder='Comment'
+                        leftIcon={{ type: 'font-awesome', name: 'comment-o' }}
+                        leftIconContainerStyle={{ paddingRight: 10 }}
+                        onChangeText={( text ) => setText(text)}
+                        value={text}
+                    />
+                </View>
+                <View>
+                    <Button 
+                        title='Submit'
+                        color='#5637DD'
+                        onPress={() => {
+                            handleSubmit();
+                            resetForm();
+                        }}
+                    />
+                </View>
                 <View style={{ margin: 10 }}>
                     <Button
-                        onPress={() => setShowModal(!showModal)}
+                        onPress={() => {
+                            setShowModal(!showModal)
+                            resetForm();
+                        }}
                         color='#808080'
                         title='Cancel'
                     />
@@ -80,6 +143,10 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         paddingHorizontal: 20,
         backgroundColor: '#fff'
+    },
+    modal: {
+        justifyContent: 'center',
+        margin: 20
     }
 });
 
