8379d33fd1a4abc31b53bdae3af4c95e1ce6bc99
aarontorresweb <aarontorresweb@gmail.com>
2022-08-05 23:39:46 -0700

    Week 3 Exercises Finished

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,2 +1,5 @@
+// Command to run JSON server:
+// json-server -H 0.0.0.0 --watch db.json -p 3001 -d 2000
+
 export const baseUrl = 'http://192.168.0.114:3001/'
 // export const baseUrl = 'https://aaronmichael.herokuapp.com/nucamp-data/';
