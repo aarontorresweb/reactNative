91f48665a07bebe4935320c095fd2c712924308f
aarontorresweb <aarontorresweb@gmail.com>
2022-08-12 21:06:56 -0700

    Social Sharing 3 Exercise: Social Sharing

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -12,7 +12,6 @@ import { Picker } from '@react-native-picker/picker';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
 import * as Notifications from 'expo-notifications';
-import * as MailComposer from 'expo-mail-composer';
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
